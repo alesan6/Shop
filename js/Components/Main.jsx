8ac7aae74f02cd3e25d1618ca1905da1a98078f4
Marek Rychlicki <krychu9@gmail.com>
2017-05-09 15:35:08 +0200

    Done main and products page

---
+++ js/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class Main extends React.Component {
+    render() {
+        return <div>
+            <div>
+                <h3 className="hidden-xs-block text-center">Shopping for ordinary men. <span className="label label-default hidden-xs-block text-center"> Just enter elegance world!</span></h3>
+                <div> <br/></div>
+                <img className="img-responsive img-rounded" src='./welcome.jpeg'>
+                </img>
+                <div> <br/></div>
+            </div>
+        </div>
+    }
+}
+
+export default Main
